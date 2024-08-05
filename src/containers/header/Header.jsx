6f9320bc82a4487fb268cfd0f5db63c3c6c809ae
milan petrovic <milan030820003@gmail.com>
2024-08-05 12:09:49 +0200

    backup

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import people from "../../assets/people.png";
-import ai from "../../assets/ai.png";
+import Spline from '@splinetool/react-spline';
+
 
 const Header = () => {
+
+  const [error, setError] = useState(null);
+
+  const handleSplineError = (error) => {
+    setError(error);
+    console.log(error);
+  };
+
   return (
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
@@ -25,9 +34,16 @@ const Header = () => {
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
       </div>
-        <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
-        </div>
+      <div className="gpt3__header-image">
+        {error ? (
+          <div>Error loading Spline scene: {error.message}</div>
+        ) : (
+          <Spline
+            scene="https://prod.spline.design/ktJ2btuQS0Vq2pgF/scene.splinecode"
+            onError={handleSplineError}
+          />
+        )}
+      </div>
     </div>
   );
 };
